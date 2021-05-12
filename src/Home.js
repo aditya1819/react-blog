d83c340fcbf5fc1bfc40a7c0a1a5201886657487
aditya1819 <shahare.aditya@gmail.com>
2021-05-12 20:09:53 +0530

    basic props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
+
 
 const Home = () => {
 
@@ -10,13 +12,7 @@ const Home = () => {
 
 	return ( 
 		<div className="home">
-			{ blogs.map((blog) =>(
-				<div className="blog-preview" key={blog.id}>
-					<h2>{blog.title}</h2>
-					<p>Author : {blog.author}</p>
-					<br></br>
-				</div>
-			))}
+			<BlogList blogs={blogs} title="All blogs" />
 
 		</div>    
 	);
