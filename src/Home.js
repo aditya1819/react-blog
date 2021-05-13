3ddd829802a7cf9bdfebed1bbb7e3ae278201b36
aditya1819 <shahare.aditya@gmail.com>
2021-05-13 23:23:38 +0530

    handling errors

--- src/Home.js
+++ src/Home.js
@@ -5,19 +5,32 @@ import BlogList from './BlogList';
 const Home = () => {
 
 	const [blogs, setBlogs] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		fetch('http://localhost:8000/blog')
 		.then(res => {
+			if(!res.ok) {
+				throw Error('Could not fetch from server :( ');
+			}
 			return res.json()
-		}).then(data => {
-			console.log(data);
+		})
+		.then(data => {
 			setBlogs(data);
+			setIsLoading(false);
+			setError(null);
+		})
+		.catch(err => {
+			setIsLoading(false);
+			setError(err.message);
 		})
 	}, [])
 
 	return ( 
 		<div className="home">
+			{error && <div> { error } </div>}
+			{isLoading && <div>Loading ... </div>}
 			{blogs && <BlogList blogs={blogs} title="All blogs" />}
 		</div>    
 	);
